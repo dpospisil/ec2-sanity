7b446d3e99e98040b09cf1368f27398ade22e2a8
Dominik Pospisil <dpospisi@redhat.com>
2012-11-08 10:51:45 +0100

    Updates.

--- src/test/groovy/com/redhat/eap6/ec2/AMISanityTest.groovy
+++ src/test/groovy/com/redhat/eap6/ec2/AMISanityTest.groovy
@@ -9,17 +9,27 @@ class AMISanityTest extends GroovyTestCase
     
     void testJBossEC2_EAP_RPM() {
         println "Testing jboss-ec2-eap RPM."
-        def ec2rpm = exec("rpm -ql jboss-ec2-eap")
+        println "**************************"
+        def ec2rpm = execForOutput("rpm -ql jboss-ec2-eap")
         println "RPM content: " + ec2rpm
         
         assertTrue(ec2rpm.contains("standalone-ec2-ha.xml"))
         assertTrue(ec2rpm.contains("standalone-mod_cluster-ec2-ha.xml"))
     }
 
-    String exec(cmd) {
+    void testYumCheck() {
+        assertEquals(0, execForExitValue("yum check all"))
+    }
+    
+    String execForOutput(cmd) {
         def proc = cmd.execute()
         proc.waitFor()
         assert proc.exitValue() == 0
         return proc.in.text
     }
+    
+    int execForExitValue(cmd) {
+        def proc = cmd.execute()
+        proc.waitFor()
+        return proc.exitValue()
 }
