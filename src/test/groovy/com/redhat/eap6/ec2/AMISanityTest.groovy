c6a6e5fdd1da7154503d066243682f201bf4dc77
Dominik Pospisil <dpospisi@redhat.com>
2012-11-08 10:17:40 +0100

    Initial Commit.

---
+++ src/test/groovy/com/redhat/eap6/ec2/AMISanityTest.groovy
@@ -0,0 +1,13 @@
+//
+// Generated from archetype; please customize.
+//
+
+package com.redhat.eap6.ec2
+
+class AMISanityTest
+    extends GroovyTestCase
+{
+    void testPass() {
+        assert 1 == 1
+    }
+}
