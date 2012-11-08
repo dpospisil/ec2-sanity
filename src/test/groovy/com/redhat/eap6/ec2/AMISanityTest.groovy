3e9dcdd3682d7d7b9eb39e114b10aef3e974667c
Dominik Pospisil <dpospisi@redhat.com>
2012-11-08 10:35:46 +0100

    Added 1st test.

--- src/test/groovy/com/redhat/eap6/ec2/AMISanityTest.groovy
+++ src/test/groovy/com/redhat/eap6/ec2/AMISanityTest.groovy
@@ -4,10 +4,21 @@
 
 package com.redhat.eap6.ec2
 
-class AMISanityTest
-    extends GroovyTestCase
+class AMISanityTest extends GroovyTestCase
 {
-    void testPass() {
-        assert 1 == 1
+    
+    void testJBossEC2_EAP_RPM() {
+        print "Testing jboss-ec2-eap RPM."
+        def ec2rpm = exec("rpm -ql jboss-ec2-eap")
+
+        assertTrue(ec2rpm.contains("standalone-ec2-ha.xml"))
+        assertTrue(ec2rpm.contains("standalone-mod_cluster-ec2-ha.xml"))
+    }
+
+    String exec(cmd) {
+        def proc = cmd.execute()
+        proc.waitFor()
+        assert proc.exitValue() == 0
+        return proc.in.text
     }
 }
